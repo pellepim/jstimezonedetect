ca8cdaf6a60b679076aa7d856de8e59c880e0882
Shoeb Khan <skhanbit@gmail.com>
2019-05-08 11:39:58 +0530

    After recent Windows upgrade jstz library is failing on Windows server 2012 R2 and Windows 8.1. Made changes in get_from_internationalization_api

--- jstz.main.js
+++ jstz.main.js
@@ -101,7 +101,7 @@ var jstz = (function () {
          */
         get_from_internationalization_api = function get_from_internationalization_api() {
             var format, timezone;
-            if (typeof Intl === "undefined" || typeof Intl.DateTimeFormat === "undefined") {
+            if (!Intl || typeof Intl === "undefined" || typeof Intl.DateTimeFormat === "undefined") {
                 return;
             }
 
